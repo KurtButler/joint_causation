dab4a28e07da10aee798f0cd2bb6a4a94ee950c3
Kurt Butler <62613188+newyorkfishmarket@users.noreply.github.com>
2024-04-10 11:50:22 -0400

    Added text for the log file

--- main.m
+++ main.m
@@ -22,35 +22,43 @@ warning('off','all')
 % Running these scripts will reproduce figures from the paper
 
 % Table 1
+disp('Generating Table 1')
 table_kernels
 
 % Fig 1
 % Visualization of the local interaction function
+disp('Generating Figure 1')
 fig_local_int_fcn
 
 % Fig 2
 % Comparison of kernels
+disp('Generating Figure 2')
 Fig_kernels
 
 % Figs 3 and 4
 %   Figure 3: A sample from the Volterra model
 %   Figure 4: Comparison of the true model, the estimated MDCE from GPR,
 %   the Volterra model estimate, and the Bayes detector
+disp('Generating Figures 3 and 4')
 Fig3_4_Volterra
 
 % Fig 5
 % Robustness of the MDCE to linear confounders
+disp('Generating Figure 5')
 Fig5_confounder
 
 % Fig 6
 % Comparison of exact GPR with sparse approximation
+disp('Generating Figure 6')
 Fig6_sparse
 
 % Fig 7
 % Real data example using New Taipei City data
+disp('Generating Figure 7')
 fig_taipei
 
 
 %% Stop writing to the log file
+disp('Running is complete')
 diary 'off'
 
